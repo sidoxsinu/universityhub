816887ec1b902e0b2388e45cb0be3a5e181eb1c5
Muhammed Sinan M <muhammedsinan.m67@gmail.com>
2026-03-13 11:25:31 +0530

    Added login page

--- academic_app/urls.py
+++ academic_app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views   # ← ADD THIS
 
 urlpatterns = [
     path('', views.home, name='home'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  # ← ADD THIS
+
     path('contact/', views.contact, name='contact'),
     path('department/', views.department, name='department'),
     path('student_registration/', views.student_registration, name='student_registration'),
--- universityhub/urls.py
+++ universityhub/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('academic_app.urls')),
+    path('', include('academic_app.urls'))
 ]
